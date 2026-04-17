c06c245940c88e5a124a68bb039c0959d02df45b
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-04-17 15:27:27 +0200

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,11 @@
+<?php
+// CONFIGURACIÓN PRIVADA DE FIREBASE
+$FIREBASE_CONFIG = [
+    "apiKey" => "AIzaSyCHNrweZJBLYq-PebkSmqgBV1vGdxXKS7M",
+    "authDomain" => "hostingimage1.firebaseapp.com",
+    "projectId" => "hostingimage1",
+    "storageBucket" => "hostingimage1.firebasestorage.app",
+    "messagingSenderId" => "871172653190",
+    "appId" => "1:871172653190:web:ffae4ae75b6d99a62c1107"
+];
+?>
